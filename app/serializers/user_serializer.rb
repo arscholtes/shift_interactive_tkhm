21eb0f1b5b22c0d9782abd8d51db09e100d592d8
arscholtes <alex.scholtes@icloud.com>
2023-12-09 23:39:12 -0500

    cleaned up data a bit

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :phone, :company_name, :created_at, :updated_at
+  attributes :name, :email, :phone, :company_name, :created_at, :updated_at
 
   has_one :address
 end
